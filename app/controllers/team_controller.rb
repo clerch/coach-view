c0443dd88e47f5469d02232f2bc93654527f460d
Ryan Callaway <callaway.r@gmail.com>
2016-06-17 15:19:48 -0700

    get calendar list working

--- app/controllers/team_controller.rb
+++ app/controllers/team_controller.rb
@@ -1,3 +1,7 @@
+require 'google/apis/calendar_v3'
+require 'google/api_client/client_secrets'
+require 'json'
+
 class TeamController < ApplicationController
 
 
@@ -17,18 +21,28 @@ class TeamController < ApplicationController
 
     @data = []
 
-    @players.each do |players|
+    @players.each do |player|
       data << {
-        name: player.name
-        id: player.id
-        events: player.get_calendar
+        name: @player.name,
+        id: @player.id
       }
+    end
 
     render :json => @data
 
   end
 
+  def create_google_auth
+    client_opts = {"authorization_uri"=>"https://accounts.google.com/o/oauth2/auth?access_type=offline&client_id=1072088737400-0mufodco7n7utcg21v8mis7s407t5atq.apps.googleusercontent.com&redirect_uri=http://localhost:3000/sessions/create&response_type=code&scope=https://www.googleapis.com/auth/calendar", "token_credential_uri"=>"https://accounts.google.com/o/oauth2/token", "client_id"=>"1072088737400-0mufodco7n7utcg21v8mis7s407t5atq.apps.googleusercontent.com", "client_secret"=>nil, "scope"=>["https://www.googleapis.com/auth/calendar"], "state"=>nil, "code"=>nil, "redirect_uri"=>"http://localhost:3000/sessions/create", "username"=>nil, "password"=>nil, "issuer"=>nil, "audience"=>nil, "person"=>nil, "expiry"=>60, "expires_at"=>1466205140, "signing_key"=>nil, "refresh_token"=>nil, "access_token"=>"ya29.CjAEA0MPl10etWURZ5YuEBSW82HjmVa6r71REk39Y5h3frLg5XqkKZEvW5oWjgW4dqE", "id_token"=>nil, "extension_parameters"=>{}}
+    auth_client = Signet::OAuth2::Client.new(client_opts)
+    service = Google::Apis::CalendarV3::CalendarService.new
 
+    service.client_options.application_name = 'Ryan testing Google Calendar API'
+    service.authorization = auth_client
+
+    calendar_list = service.list_calendar_lists
+    puts calendar_list
+  end
 
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -33,12 +33,6 @@ ActiveRecord::Schema.define(version: 20160616222513) do
     t.datetime "updated_at",    null: false
   end
 
-  create_table "roles", force: :cascade do |t|
-    t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "teams", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
