2aa54a27b82fe71243505c76cd7b46b449ae4e6c
clerch <lerchc@gmail.com>
2016-06-17 12:32:24 -0700

    starting in controllers

--- app/controllers/team_controller.rb
+++ app/controllers/team_controller.rb
@@ -8,6 +8,16 @@ class TeamController < ApplicationController
 
   def show
     @team = Team.find(params[:id])
+    @resources = @team.resources.order(updated_at: :asc)
+    @players = @team.users.where(coach: false)
+    @coach = @team.users.where(coach: true)
+
+
+
+
+
+
+    
   end
 
 
