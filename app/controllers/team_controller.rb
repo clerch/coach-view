c0a9ee3c66d0588cec1f8601eee7d4c9724899f1
Ryan Callaway <callaway.r@gmail.com>
2016-06-18 13:34:47 -0700

    refresh tokens in team_controller

--- app/controllers/team_controller.rb
+++ app/controllers/team_controller.rb
@@ -62,36 +62,90 @@ class TeamController < ApplicationController
   end
 
 
-  def fetch_events_list(calendar)
+  def fetch_events_list(player)
   
       client_opts = {"authorization_uri"=>"https://accounts.google.com/o/oauth2/auth?access_type=offline&client_id=1072088737400-0mufodco7n7utcg21v8mis7s407t5atq.apps.googleusercontent.com&redirect_uri=http://localhost:3000/sessions/create&response_type=code&scope=https://www.googleapis.com/auth/calendar", 
       "token_credential_uri"=>"https://accounts.google.com/o/oauth2/token", 
       "client_id"=>ENV['GOOGLE_API_CLIENT_ID'], 
-      "client_secret"=>nil, "scope"=>["https://www.googleapis.com/auth/calendar"], 
+      "client_secret"=>ENV['GOOGLE_API_CLIENT_SECRET'], 
+      "scope"=>["https://www.googleapis.com/auth/calendar"], 
       "state"=>nil, 
       "code"=>nil, 
-      "redirect_uri"=>"http://localhost:3000/sessions/create", 
+      "redirect_uri"=>nil, 
       "username"=>nil, 
       "password"=>nil, 
       "issuer"=>nil, 
       "audience"=>nil, 
       "person"=>nil, 
       "expiry"=>60, 
-      "expires_at"=>1466211285, 
+      "expires_at"=>player.expires_at, 
       "signing_key"=>nil, 
-      "refresh_token"=>nil, 
-      "access_token"=>"ya29.CjAEA7YngxJckxCrRIEdNCokZgh028kPvQZODZ3i70vhi7woaKsg1iILePvy0fv5W0Y", 
+      "refresh_token"=>player.refresh_token, 
+      "access_token"=>nil, 
       "id_token"=>nil, 
       "extension_parameters"=>{}}
 
-    auth_client = Signet::OAuth2::Client.new(client_opts)
+      CLIENT_SECRETS = Google::APIClient::ClientSecrets.new(client_opts)
+
+      # Build an authorization object from the client secrets.
+      # authorization is a Client object
+      auth_client = CLIENT_SECRETS.to_authorization
+      auth_client.update_token!(
+        # :access_token => session['credentials']['access_token'],
+        :refresh_token => player.refresh_token
+      )
+      
+
+    # before do
+    #   # Ensure user has authorized the app
+    #   unless user_credentials.access_token || request.path_info =~ /\A\/oauth2/
+    #     redirect to('/oauth2authorize')
+    #   end
+    # end
+
+      # # Execute the profile API call.
+      # get_profile = lambda do
+      #   client.execute(
+      #     :api_method => plus_api.people.get,
+      #     :parameters => {'userId' => 'me'},
+      #     :authorization => authorization
+      #   )
+      # end
+      # profile_result = get_profile.call()
+      # if profile_result.status == 401
+      #   # The access token expired, fetch a new one and retry once.
+      #   client.authorization.fetch_access_token!
+      #   profile_result = get_profile.call()
+      # end
+
+      # # Execute the activities API call.
+      # get_activities = lambda do
+      #   client.execute(
+      #     :api_method => plus_api.activities.list,
+      #     :parameters => {'userId' => 'me', 'collection' => 'public'},
+      #     :authorization => authorization
+      #   )
+      # end
+      # activities_result = get_activities.call()
+      # if activities_result.status == 401
+      #   # The access token expired, fetch a new one and retry once.
+      #   client.authorization.fetch_access_token!
+      #   activities_result = get_activities.call()
+      # end
+      
+      # erb :whoami, :locals => {
+      #   :profile => profile_result.data,
+      #   :post => activities_result.data.items.first
+      # }
+
+    # auth_client = Signet::OAuth2::Client.new(client_opts)
     service = Google::Apis::CalendarV3::CalendarService.new
 
     service.client_options.application_name = 'Ryan testing Google Calendar API'
     service.authorization = auth_client
 
     events_list = service.list_events(
-      calendar, 
+      player.calendar_id, 
       always_include_email: nil, 
       i_cal_uid: nil, 
       max_attendees: nil, 
