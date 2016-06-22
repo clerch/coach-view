d7d47b5e305ea52b048c714ad1cddc99df47aaca
Sterling <sterling.nostedt@gmail.com>
2016-06-21 17:33:04 -0700

    playerviewcontainer fixed and includes table and calendar again

--- src/js/containers/PlayerViewContainer.jsx
+++ src/js/containers/PlayerViewContainer.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 // import GradeContainer from './GradeContainer.jsx'
 import NavBar from '../components/NavBar.jsx'
 import TableEdit from 'material-ui-table-edit'
+import BigCalendarContainer from '../containers/BigCalendarContainer.jsx';
+import SettingsWindowContainer from '../containers/SettingsWindowContainer.jsx';
+import ClassListContainer from './ClassListContainer.jsx'
 import SnackbarContainer from './SnackbarContainer.jsx'
 
 export default class PlayerViewContainer extends React.Component {
@@ -9,7 +12,10 @@ export default class PlayerViewContainer extends React.Component {
     return(
       <div>
         <NavBar/>
-        <TableEdit/>
+        <ClassListContainer/>
+        <div className="player-calendar">
+          <BigCalendarContainer/>
+        </div>
         <div>
           <SnackbarContainer/>
         </div>
