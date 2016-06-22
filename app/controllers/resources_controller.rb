af810961df54e50f440c75252492503b7f031803
James Leddy <jwleddy@gmail.com>
2016-06-21 18:14:12 -0700

    fixed resource controller

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -30,11 +30,12 @@ class ResourcesController < ApplicationController
 
   def update
     @resource = Resource.find(params[:id])
-    @resource = Resource.update(
+    @resource.update(
       resource_type: params[:resource_type],
       content: params[:content],
       name: params[:name]
-    )    
+    )
+    render :nothing => true, :status => 200
   end
 
 
--- src/js/lib/postingFunctions.js
+++ src/js/lib/postingFunctions.js
@@ -1,5 +1,9 @@
 import { HOST_NAME } from './constants'
 
+var headers = new Headers()
+headers.append("Content-Type", "application/json")
+headers.append("Accept", "application/json")
+
 export function updateResource(resource) {
 
   var body = {
@@ -7,7 +11,11 @@ export function updateResource(resource) {
     content: resource.content,
     resource_type: resource.resource_type
   }
-  var options = {method:"PUT", body: JSON.stringify(body)}
+  var options = {
+    method:"PUT",
+    body: JSON.stringify(body),
+    headers: headers
+  }
 
   fetch(HOST_NAME + "team/" + resource.team_id + "/resources/" + resource.id, options)
     .then(function(res){
