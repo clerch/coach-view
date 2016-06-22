4718b15e88996be3189b7d9c1557712fbbf807b1
Sterling <sterling.nostedt@gmail.com>
2016-06-22 15:32:02 -0700

    title in navbar, basic footer and starting to format the editor box

--- src/js/components/NavBar.jsx
+++ src/js/components/NavBar.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import TeamSettingsButton from './TeamSettingsButton.jsx'
+import Footer from './Footer.jsx'
 
 export default class NavBar extends React.Component {
   render() {
     return(
+      <div>
       <nav
         className="navBar"
       >
       <TeamSettingsButton/>
+        <div className="navBarTitle">Lineup</div>
       </nav>
+      <Footer />
+      </div>
     )
   }
 }
